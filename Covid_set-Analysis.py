f2e64656fa8d8aaf47eaf8e5fb9d67efc305ed80
Aaronj88 <155629991+Aaronj88@users.noreply.github.com>
2024-12-04 17:52:30 +0100

    Update Covid_set-Analysis.py

--- Covid_set-Analysis.py
+++ Covid_set-Analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.express as px
+import plotly.express as px #simpler, dynamic/interactive version of matplotlib. built on top of graph objects
+import plotly.graph_objects as go #more customizable, less simple
 
 c = pd.read_csv("covid_data.csv")
 covid = c[["Province_State","Country_Region","Last_Update","Confirmed","Recovered","Deaths","Active"]]
@@ -21,4 +22,21 @@ fig1.write_html("figure.html",auto_open=False)
 
 fig2 = px.bar(topten,x=topten.index,y="Confirmed",height=650,color=topten.index,title="Which Countries Were Most Affected by Covid 19")
 fig2.write_html("figure2.html",auto_open=True)
+
+
+
+
+us = covid[covid["Country_Region"]=="US"]
+a = us.sort_values("Confirmed",ascending=False)
+top_us = a.head(10)
+
+fig3 = go.Figure(data=[
+    go.Bar(name="Confirmed Cases",y=top_us["Province_State"],x=top_us["Confirmed"],orientation="h"),
+    go.Bar(name="Recoveries",y=top_us["Province_State"],x=top_us["Recovered"],orientation="h"),
+    go.Bar(name="Deaths",y=top_us["Province_State"],x=top_us["Deaths"],orientation="h")
+    ])
+
+fig3.update_layout(title="How the USA were affected by Covid-19",height=850)
+fig3.write_html("figure3.html",auto_open=True)
+
 
